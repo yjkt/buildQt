8e8aa1fc6f27c66add1b365936c7f618cfe3027c
yjkt <mpbgp@qq.com>
2025-09-25 11:51:03 +0800

    Update build-Qt6mingw1310_64.cmd

--- buildQt6/build-Qt6mingw1310_64.cmd
+++ buildQt6/build-Qt6mingw1310_64.cmd
@@ -8,7 +8,7 @@ SET QT_VERSION=6.9.2
 SET MinGW_VERSION=mingw1310_64
 
 :: 设置编译器和Ninja
-SET PATH=D:\a\buildQt\Tools\mingw1310_64\bin;D:\a\buildQt\ninja;%PATH%
+SET PATH=D:\a\buildQt\mingw1310_64\bin;D:\a\buildQt\ninja;%PATH%
 
 :: 设置Qt文件夹路径
 SET QT_PATH=D:\a\buildQt\Qt
